8e60c48214647c4a5d92d3358c61512d22a7959c
simbastart001 <simarindwa@gmail.com>
2023-11-11 21:24:23 +0200

    App complete with validation in place

--- app/src/main/java/com/udacity/shoestore/ui/login/LoginFragment.kt
+++ app/src/main/java/com/udacity/shoestore/ui/login/LoginFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.fragment.findNavController
 import com.udacity.shoestore.R
@@ -26,14 +27,39 @@ class LoginFragment : Fragment() {
         )
 
         binding.loginBtn.setOnClickListener {
-            findNavController().navigate(LoginFragmentDirections.actionLoginFragmentToWelcomeFragment())
+//            TODO @SimbaStart:     add validation for email and nullOrEmpty check
+            if (isValidInput()) {
+                Toast.makeText(requireContext(), "Login successful", Toast.LENGTH_SHORT).show()
+                findNavController().navigate(LoginFragmentDirections.actionLoginFragmentToWelcomeFragment())
+            } else {
+                Toast.makeText(requireContext(), "Invalid email or password", Toast.LENGTH_SHORT).show()
+            }
         }
         binding.signupBtn.setOnClickListener {
+            if (isValidInput()) {
+                Toast.makeText(requireContext(), "Sign Up here", Toast.LENGTH_SHORT).show()
+                findNavController().navigate(LoginFragmentDirections.actionLoginFragmentToWelcomeFragment())
+            } else {
+                // Show an error message or handle invalid input
+                Toast.makeText(requireContext(), "Invalid email or password", Toast.LENGTH_SHORT).show()
+            }
             findNavController().navigate(LoginFragmentDirections.actionLoginFragmentToWelcomeFragment())
         }
-
 
         return binding.root
     }
 
+    private fun isValidInput(): Boolean {
+        val email = binding.emailAddressEdittext.text.toString()
+        val password = binding.editTextTextPassword.text.toString()
+
+        if (isValidEmail(email) && password.isNotEmpty()) {
+            return true
+        }
+        return false
+    }
+
+    private fun isValidEmail(email: String): Boolean {
+        return android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()
+    }
 }
